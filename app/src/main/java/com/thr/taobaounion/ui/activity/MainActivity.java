26d7e3dc02e2f9f4f148b2f478601cb0250e2b4c
tang <10948849@qq.com>
2023-01-24 15:37:32 +0800

    实现碎片3个页面的点击替换

--- app/src/main/java/com/thr/taobaounion/ui/activity/MainActivity.java
+++ app/src/main/java/com/thr/taobaounion/ui/activity/MainActivity.java
@@ -12,7 +12,10 @@ import android.view.MenuItem;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.thr.taobaounion.R;
+import com.thr.taobaounion.base.BaseFragment;
 import com.thr.taobaounion.ui.fragment.HomeFragment;
+import com.thr.taobaounion.ui.fragment.SaleFragment;
+import com.thr.taobaounion.ui.fragment.SearchFragment;
 import com.thr.taobaounion.utils.LogUtils;
 
 import butterknife.BindView;
@@ -20,38 +23,50 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
     @BindView(R.id.main_navigation_bar)
     public BottomNavigationView navigationView;//底部导航栏
+    private HomeFragment homeFragment;
+    private SaleFragment saleFragment;
+    private SearchFragment searchFragment;//碎片
+    private FragmentManager fm;//碎片管理
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        initView();
         initListener();
+        initFragment();
+    }
+
+    private void initFragment() {
+        homeFragment = new HomeFragment();
+        saleFragment = new SaleFragment();
+        searchFragment = new SearchFragment();
+        fm = getSupportFragmentManager();
+        switchFragment(homeFragment);
     }
 
     private void initListener() {
         navigationView.setOnItemSelectedListener(item -> {
             if (item.getItemId() == R.id.home) {
                 LogUtils.d(this.getClass(), "切换到首页");
+                switchFragment(homeFragment);
             } else if (item.getItemId() == R.id.sale) {
                 LogUtils.d(this.getClass(), "切换到特惠");
+                switchFragment(saleFragment);
             } else if (item.getItemId() == R.id.search) {
                 LogUtils.d(this.getClass(), "切换到搜索");
+                switchFragment(searchFragment);
             }
             return true;
         });
     }
 
-    private void initView() {
-
-        HomeFragment homeFragment = new HomeFragment();
-        FragmentManager fm = getSupportFragmentManager();
+    private void switchFragment(BaseFragment targetFragment) {
         FragmentTransaction transaction = fm.beginTransaction();
-        transaction.replace(R.id.main_page_container, homeFragment);
+        transaction.replace(R.id.main_page_container, targetFragment);
         transaction.commit();
     }
 }
